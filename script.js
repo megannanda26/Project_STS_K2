54433930bca3b3490a0f1e08d6892964c9aec17e
ahmadadjiemegananda <megannanda26@gmail.com>
2026-03-26 14:23:50 +0700

    Hero, testimoni & form

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // document.onload(alert("Selamat datang di web Donasi Barang"))
+let form = document.getElementById("form-donasi")
+let open = document.getElementById("open")
+open.addEventListener("click", function() {
+    form.removeAttribute("hidden")
+} )
+let close = document.getElementById("close")
+close.addEventListener("click", function() {
+    form.setAttribute("hidden", true)
+})
