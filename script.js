173b321c18bc216fe27386eb68434873bf8bbbc7
ahmadadjiemegananda <megannanda26@gmail.com>
2026-04-03 22:13:56 +0700

    header,about ++

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ let close = document.getElementById("close")
 close.addEventListener("click", function() {
     form.setAttribute("hidden", true)
 })
+
+const hero = [
+    "img/hero1.jpg",
+    "img/hero2.jpg",
+    "img/hero3.jpg",
+    "img/hero4.jpg"
+]
+
+let intervalHero = 0
+function changeHero() {
+    document.querySelector("header").style.backgroundImage = `url(${hero[intervalHero]})`
+    intervalHero = (intervalHero + 1) % hero.length
+}
+
+setInterval(changeHero, 2000)
+
+changeHero()
